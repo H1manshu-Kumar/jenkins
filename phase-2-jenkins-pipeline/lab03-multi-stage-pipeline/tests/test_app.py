2e1558952259323d49e21f25f9ee379573e13594
H1manshu-Kumar <himanshu.kumar244@gmail.com>
2026-03-31 23:20:01 +0530

    fix(lab-03): fix failed unit test :boom: :hammer:

--- phase-2-jenkins-pipeline/lab03-multi-stage-pipeline/tests/test_app.py
+++ phase-2-jenkins-pipeline/lab03-multi-stage-pipeline/tests/test_app.py
@@ -3,8 +3,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from app import app
-from models import init_db
+from devpulse.app import app
+from devpulse.models import init_db
 
 
 @pytest.fixture
